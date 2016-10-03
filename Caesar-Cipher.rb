98861105b7789390985a43ec978197d542141221
AstPetr <astpetr@gmail.com>
2016-10-03 18:39:19 +0300

    added functions

--- Caesar-Cipher.rb
+++ Caesar-Cipher.rb
@@ -1,20 +1,24 @@
 # Cezario šifras
 # Pakeičiamos raidės nurodytu poslinkiu  
 
+def caesar_cipher (words, shift)
+	i = 0
+	while i<words.length do
+		if words[i].match(/\w/) != nil
+			ind = words[i].ord+shift
+			if (words[i] == words[i].upcase && ind > 90) || (words[i] == words[i].downcase && ind > 122)
+				words[i] = (ind-26).chr
+			else
+				words[i] = ind.chr
+			end
+		end
+	i+= 1
+	end
+end
+
 puts "Parašyk ką nors!"
 words = gets.chomp
 puts "Poslinkis:"
 shift = gets.chomp.to_i
-i = 0
-while i<words.length do
-	if words[i].match(/\w/) != nil
-		ind = words[i].ord+shift
-		if (words[i] == words[i].upcase && ind > 90) || (words[i] == words[i].downcase && ind > 122)
-			words[i] = (ind-26).chr
-		else
-			words[i] = ind.chr
-		end
-	end
-	i+= 1
-end
+caesar_cipher(words, shift)
 puts words
--- Substrings.rb
+++ Substrings.rb
@@ -2,20 +2,23 @@
 # dictionary) as the second argument. It should return a hash listing each substring (case insensitive) that was found in 
 # the original string and how many times it was found.
 
+def substrings (words, dictionary)
+	word_hash = {}
+	dictionary.each do |i|
+		nr = 0
+		words.each do |word|
+			if word[i] != nil
+				nr += word.scan(/#{i}/).count
+				word_hash[i] = nr
+			end
+		end
+	end
+	word_hash
+end
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 puts "Parašyk ką nors!"
 words = gets.chomp.downcase.split(" ")
-word_hash = {}
+word_hash = substrings(words, dictionary)
 # words = "Howdy partner, sit down! How's it going?"
-
-dictionary.each do |i|
-	nr = 0
-	words.each do |word|
-		if word[i] != nil
-			nr += word.scan(/#{i}/).count
-			word_hash[i] = nr
-		end
-	end
-end
-
 puts word_hash
